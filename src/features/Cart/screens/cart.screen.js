472103d694b993d013790996860474768cf2b4f9
imdnk <imdnk96@gmail.com>
2022-02-05 19:40:48 +0530

    Initial with navigation

---
+++ src/features/Cart/screens/cart.screen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const CartScreen = () => {
+  return(<Text>    
+      Cart Screen here.....
+  </Text>)
+};
---
+++ src/infrastructure/navigations/app.navigator.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import {Assets, View, Icon, Slider, Switch, GradientSlider} from 'react-native-ui-lib';
+
+import { HomeScreen } from "../../features/Home/screens/home.screen";
+import { CartScreen } from "../../features/Cart/screens/cart.screen";
+
+const Tab = createBottomTabNavigator();
+
+
+
+export const AppNavigator = () => {
+ return(
+    <Tab.Navigator>
+    <Tab.Screen name="Home" component={HomeScreen} options={{headerShown: false }}/>
+    <Tab.Screen name="Cart" component={CartScreen}  options={{headerShown: false }} />
+  </Tab.Navigator>
+ );
+};
